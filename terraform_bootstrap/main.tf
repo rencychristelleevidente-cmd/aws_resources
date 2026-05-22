a9b0d15102e703e39327c5298dc42f15d9a4b2e2
Rency Evidente <rencychristelle.evidente@gmail.com>
2026-05-22 14:38:00 +0800

    comment out s3 and dynamo

--- terraform_bootstrap/main.tf
+++ terraform_bootstrap/main.tf
@@ -1,15 +1,15 @@
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = var.bucket_name
-}
+# resource "aws_s3_bucket" "terraform_state" {
+#   bucket = var.bucket_name
+# }
 
-resource "aws_dynamodb_table" "terraform_lock" {
-  name = var.lock_table_name
+# resource "aws_dynamodb_table" "terraform_lock" {
+#   name = var.lock_table_name
 
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
+#   billing_mode = "PAY_PER_REQUEST"
+#   hash_key     = "LockID"
 
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
+#   attribute {
+#     name = "LockID"
+#     type = "S"
+#   }
+# }
